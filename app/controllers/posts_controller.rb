577692fdaab34ce0551cdf7c0511e9a4d4f1154f
Lena Aiupova <e.ayupova@gmail.com>
2020-10-06 23:33:58 -0400

    Created the new Route for pages path, a controller for redering a view. Created a model class to hold pages data. Added list of pages using cobsole. Populated the view assigning the attributes to variable and loop over each of the page object

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,7 +69,6 @@ class PostsController < ApplicationController
     end
 
     # Only allow a list of trusted parameters through.
-	#
     def post_params
       params.require(:post).permit(:title, :body)
     end
