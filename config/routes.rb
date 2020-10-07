84e91233dfc401a90a8c7dbd2a87b12f93edcdfa
Lena Aiupova <e.ayupova@gmail.com>
2020-10-07 02:03:44 -0400

    Set up link_to method to view individual Pages objects. Used Path Helper. Set up a route to the show method on thr PageController. Created a file for showing the view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages', to: 'pages#index'
+  #get '/pages/:id', to: 'pages#show'
+  #Name a Route
+  get '/pages/:id', to: 'pages#show', as: 'page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
